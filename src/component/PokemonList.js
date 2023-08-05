90be18ce57d7accdf6254cff0ecd72b31dcdc3ca
kittisak191 <97100702+kittisak191@users.noreply.github.com>
2023-08-05 19:48:13 +0700

    first commit

--- src/component/PokemonList.js
+++ src/component/PokemonList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./PokemonList.css";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function PokemonList() {
   const [data, setData] = useState(null);
@@ -42,41 +43,47 @@ function PokemonList() {
   }, []);
 
   return (
-    <div>
-      {" "}
-      <div className="filters">
-        <input
-          type="text"
-          placeholder="Search Pokemon"
-          value={searchTerm}
-          onChange={handleSearchChange}
-        />
-        <select value={filteredType} onChange={handleFilterChange}>
-          <option value="">All Types</option>
-          <option value="normal">Normal</option>
-          <option value="fire">Fire</option>
-          <option value="water">Water</option>
-          <option value="electric">Electric</option>
-          <option value="grass">Grass</option>
-          <option value="ice">Ice</option>
-          <option value="fighting">Fighting</option>
-          <option value="poison">Poison</option>
-          <option value="ground">Ground</option>
-          <option value="flying">Flying</option>
-          <option value="psychic">Psychic</option>
-          <option value="bug">Bug</option>
-          <option value="rock">Rock</option>
-          <option value="ghost">Ghost</option>
-          <option value="dragon">Dragon</option>
-          <option value="dark">Dark</option>
-          <option value="steel">Steel</option>
-          <option value="fairy">Fairy</option>
-        </select>
-      </div>
-      <div className="pokemon">
+    <div className="container">
+      <div className="row mt-3">
+        <div className="col-lg-12 col-md-6 col-sm-12">
+          <input
+            type="text"
+            className="form-control mb-3"
+            placeholder="Search Pokemon"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </div>
+        <div className="col-lg-12 col-md-6 col-sm-12">
+          <select
+            className="form-select mb-3"
+            value={filteredType}
+            onChange={handleFilterChange}
+          >
+            <option value="">All Types</option>
+            <option value="normal">Normal</option>
+            <option value="fire">Fire</option>
+            <option value="water">Water</option>
+            <option value="electric">Electric</option>
+            <option value="grass">Grass</option>
+            <option value="ice">Ice</option>
+            <option value="fighting">Fighting</option>
+            <option value="poison">Poison</option>
+            <option value="ground">Ground</option>
+            <option value="flying">Flying</option>
+            <option value="psychic">Psychic</option>
+            <option value="bug">Bug</option>
+            <option value="rock">Rock</option>
+            <option value="ghost">Ghost</option>
+            <option value="dragon">Dragon</option>
+            <option value="dark">Dark</option>
+            <option value="steel">Steel</option>
+            <option value="fairy">Fairy</option>
+          </select>
+        </div>
         {isLoading
           ? Array.from({ length: 180 }, (_, index) => (
-              <div key={index}>
+              <div key={index} className="col-lg-2 col-md-3 col-sm-4">
                 <h3>
                   <Skeleton />
                 </h3>
@@ -96,7 +103,7 @@ function PokemonList() {
                 pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
               )
               .map((pokemon, index) => (
-                <div key={index}>
+                <div key={index} className="col-lg-2 col-md-3 col-sm-4">
                   <h3>{pokemon.name}</h3>
                   <img src={pokemon.image} alt={pokemon.name} />
                 </div>
